5eb32d244a4d4d3c093273f09484d1fb69363737
Markus Weber <joop03@alice-dsl.net>
2017-06-21 11:02:28 +0200

    imporved code accoring to review comments.
    Fixed variable names.
    removed unused variables.
    made constant variables final.

--- PedelecApp/src/de/tum/in/www1/PedelecApp.java
+++ PedelecApp/src/de/tum/in/www1/PedelecApp.java
@@ -47,29 +47,36 @@ public class PedelecApp extends Application {
         primaryStage.show();
 		
 		reserveButton.setOnAction(new EventHandler<ActionEvent>() {
-		@Override public void handle(ActionEvent e) { 
-			System.out.println("Reserve Button clicked");
-			LocalDate XX1 = datePicker.getValue();
-			String x_1 = timeTextField.getText(), x_2 = x_1, x_3; x_2 = pedelecNametext.getText(); 
-			String x_4 = x_2;
-			x_3 = "Please confirm your reservation of " + x_2 + " at " +
-				XX1.format(DateTimeFormatter.ISO_LOCAL_DATE) + " " + timeTextField.getText(); 
-	
-			ButtonType x__4 = new Alert(AlertType.CONFIRMATION, "" +
-				x_3).showAndWait().get();
-			if (x__4 == ButtonType.OK) { Reservation r33 = new Reservation(), r34; r33.setBike(x_4);
-				PedelecActivity.java } }
-		➡ 
-			r33.setStartDate(XX1);
-			r33.setStartTime(timeTextField.getText().concat("")); 
-			r34 = r33; 
-			r34.save();
-			System.out.println("Reservation confirmed");
-		}
-}
+			@Override public void handle(ActionEvent e) { 
+				System.out.println("Reserve Button clicked");
+				LocalDate localDate = datePicker.getValue();
+
+				if(startDate != null){
+					final String time = timeTextField.getText(); 
+					final String pedelecName = pedelecNametext.getText(); 
+					final String confirmationMessage = "Please confirm your reservation of " + pedelecName + " at " +
+						localDate.format(DateTimeFormatter.ISO_LOCAL_DATE) + " " + time; 
+			
+					ButtonType alert = new Alert(AlertType.CONFIRMATION, confirmationMessage).showAndWait().get();
+					if (alert == ButtonType.OK) { 
+						Reservation newReservation = new Reservation(); 
+						newReservation.setBike(x_4);
+						PedelecActivity.java
+						newReservation.setStartDate(localDate);
+						newReservation.setStartTime(time); 
+						newReservation.setBike(pedelecName);
+						newReservation.save();
+						System.out.println("Reservation confirmed");
+					}
+					else{
+						System.out.println("Reservation cancelled");
+					}
+				}
+				else{
+					System.out.println("Please choose a valid date time");
+				}
+			}
 		});
-
-
     }
 
 	public static void main(String[] args) {
